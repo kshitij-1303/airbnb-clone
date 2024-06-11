3fa2e587c27453c1d59f132e2292e9c5dc36f198
Kshitij Deshpande <kshitijdeshpande9325@gmail.com>
2024-06-11 20:12:52 +0530

    create all routes to set up the project

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
+const path = require("path");
+const methodOverride = require("method-override");
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 main()
   .then(() => {
@@ -24,20 +31,70 @@ app.get("/", (req, res) => {
   res.send("welcome");
 });
 
-app.get("/testListing", async (req, res) => {
-  let sampleListing = new Listing({
-    title: "New home",
-    description: "Big villa with beautiful view",
-    price: 1200,
-    location: "Panaji, Goa",
-    country: "India",
-  });
+// Index route
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
+});
 
-  await sampleListing.save();
-  console.log(sampleListing);
-  res.send("Listing saved");
+// New route
+app.get("/listings/new", async (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body);
+  await newListing.save();
+  res.redirect("/listings");
+});
+
+// Show route
+app.get("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  console.log(listing);
+  res.render("listings/show.ejs", { listing });
+});
+
+// Edit route
+
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  let listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+
+// Update route
+
+app.put("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  const newListing = req.body;
+  await Listing.findByIdAndUpdate(id, newListing);
+  res.redirect("/listings");
+});
+
+// Destroy route
+
+app.delete("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  await Listing.findByIdAndDelete(id);
+  res.redirect("/listings");
 });
 
 app.listen(PORT, () => {
   console.log(`app is listening to port ${PORT}`);
 });
+
+// app.get("/testListing", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: "New home",
+//     description: "Big villa with beautiful view",
+//     price: 1200,
+//     location: "Panaji, Goa",
+//     country: "India",
+//   });
+
+//   await sampleListing.save();
+//   console.log(sampleListing);
+//   res.send("Listing saved");
+// });
